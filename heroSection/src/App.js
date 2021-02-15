6499de6ff2c5ba31816985ad945ba4fa45a1f12b
Yevhwnii <mirandoos3223@gmail.com>
2021-02-15 23:55:32 +0100

    removed import

--- heroSection/src/App.js
+++ heroSection/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { gsap, Power3 } from 'gsap';
 
 import ArrowIcon from './assets/arrow-right.svg';
